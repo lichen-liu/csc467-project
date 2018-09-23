4c9501165f1b5ac375e57be22574f85e13605306
Lichen Liu <issac.liu@mail.utoronto.ca>
2018-09-23 01:04:45 -0400

    Supports single/multi-line comments, nested comments are not tested.

--- tests/lexer_testing.c
+++ tests/lexer_testing.c
@@ -1,2 +1,38 @@
 if
 else
+ifelse
+if else
+1
+0
++1
++0
+-1
+-0
+11
+10
++11
+-10
+01
+00
++01
++00
+-01
+-00
+
+/* comment */
+
+/* comment head
+comment body
+comment tail*/
+
+/*
+/*comment in comment*/
+*/
+
+/*/*comment in comment*/*/
+
+/*
+/*
+Comment
+*/
+*/
