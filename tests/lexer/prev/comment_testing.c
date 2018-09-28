f8c68993f791556e941c961e6c241beb8bf02fed
Lichen Liu <issac.liu@mail.utoronto.ca>
2018-09-28 16:35:37 -0400

    Check dangling comment at EOF

--- tests/lexer/prev/comment_testing.c
+++ tests/lexer/prev/comment_testing.c
@@ -6,3 +6,5 @@ LEXICAL ERROR, LINE 17: Cannot find the beginning of this comment block with /*
 LEXICAL ERROR, LINE 26: Expecting closing previous comment block with */ before opening new comment block with /*
 
 LEXICAL ERROR, LINE 29: Cannot find the beginning of this comment block with /*
+
+LEXICAL ERROR, LINE 33: Comment block is not closed by */ until EOF
