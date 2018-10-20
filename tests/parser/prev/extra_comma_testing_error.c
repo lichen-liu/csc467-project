6c1e03b42c8de78b1aa6d29dff18a2e1d56007f3
Lichen Liu <issac.liu@mail.utoronto.ca>
2018-10-20 16:04:58 -0400

    Added one more test case for error.

---
+++ tests/parser/prev/extra_comma_testing_error.c
@@ -0,0 +1,7 @@
+declarations -> epsilon
+type -> int
+function_name -> lit | dp3 | rsq
+expression -> integer_literal
+arguments -> expression
+
+PARSER ERROR, LINE 2: syntax error, unexpected RPAREN
