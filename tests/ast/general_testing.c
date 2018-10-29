5ce845b3915999aed9264e942e44d1a9915edb47
Lichen Liu <issac.liu@mail.utoronto.ca>
2018-10-29 04:51:24 -0400

    Updated test case. It is beautiful.

--- tests/ast/general_testing.c
+++ tests/ast/general_testing.c
@@ -1,10 +1,15 @@
 {
     const int a = 12;
-    int b = 2;
+    int b = 4 - 6 * 2;
+    int c;
+    vec3 v3;
+
+    v3 = vec3(1., 2., 3.);
     a = (a + b);
+    v3[1] = 5.5;
 
     if(a == 2) {
         b = 2;
-    } else 
+    } else
         b = 3;
 }
