30c9a7222d672101031c53567adb12d4d0b4f428
ganyang <gabriel.yang@mail.utoronto.ca>
2018-11-10 11:22:08 -0500

    2 tests

--- tests/semantic_const/prev/simple_int_testing.c
+++ tests/semantic_const/prev/simple_int_testing.c
@@ -1,6 +1,7 @@
 Info: Optimization for declaration of const-qualified symbol 'a' of type 'const int' successful at Line 2:5 to Line 2:21.
 Info: Optimization for declaration of const-qualified symbol 'b' of type 'const int' successful at Line 3:5 to Line 3:34.
 Info: Optimization for declaration of const-qualified symbol 'c' of type 'const int' successful at Line 4:5 to Line 4:25.
+Info: Optimization for declaration of const-qualified symbol 'd' of type 'const int' successful at Line 5:5 to Line 5:22.
 (SCOPE
     (DECLARATIONS
         (DECLARATION gl_FragColor result vec4)
@@ -19,6 +20,7 @@ Info: Optimization for declaration of const-qualified symbol 'c' of type 'const 
         (DECLARATION a const int 3)
         (DECLARATION b const int 30)
         (DECLARATION c const int 31)
+        (DECLARATION d const int -3)
     )
     (STATEMENTS
     )
